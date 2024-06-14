db426318704ae7d55b0820e5050219eeb74ac48d
jyungtong <huntthetux@gmail.com>
2024-06-14 22:41:54 +0800

    add lockscreen

--- config.def.h
+++ config.def.h
@@ -59,6 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *lockscreen[]  = { "slock", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -99,6 +100,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ Mod1Mask|ControlMask,         XK_q,      spawn,          {.v = lockscreen } }, 
 };
 
 /* button definitions */
