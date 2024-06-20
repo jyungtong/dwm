e9d97a2d01f3d6eddd1916b33c94086ffef53d1e
jyungtong <huntthetux@gmail.com>
2024-06-20 10:38:33 +0800

    adjust set_volume to 5%

--- addons/set_volume.sh
+++ addons/set_volume.sh
@@ -8,10 +8,10 @@ fi
 
 case $1 in
     up)
-        pactl set-sink-volume @DEFAULT_SINK@ +10%
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
         ;;
     down)
-        pactl set-sink-volume @DEFAULT_SINK@ -10%
+        pactl set-sink-volume @DEFAULT_SINK@ -5%
         ;;
     mute)
         pactl set-sink-mute @DEFAULT_SINK@ toggle
