0e64c640a3d805c0bf1501a753f08c03692eb3f0
jyungtong <huntthetux@gmail.com>
2024-06-23 21:40:16 +0800

    fix layout array

--- config.def.h
+++ config.def.h
@@ -49,6 +49,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	// { "[@]",      spiral },
 	// { "[\\]",     dwindle },
@@ -61,7 +62,6 @@ static const Layout layouts[] = {
 	// { ":::",      gaplessgrid },
 	// { "|M|",      centeredmaster },
 	// { ">M>",      centeredfloatingmaster },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
