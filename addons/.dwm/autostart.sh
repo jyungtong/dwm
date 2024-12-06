c1de7480058c96eec3934ae8863595e934173a99
jyungtong <huntthetux@gmail.com>
2024-12-06 11:18:42 +0800

    use ctrl to switch tag and adjust autostart.sh

--- addons/.dwm/autostart.sh
+++ addons/.dwm/autostart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-picom --vsync &
+picom --vsync --backend=glx &
 xss-lock -- slock &
 xset s 600 &
 killall dwmblocks || true && dwmblocks &
 dunst &
 fcitx5 &
+xrandr --output HDMI-1 --mode 1920x1080 --rate 74.97
