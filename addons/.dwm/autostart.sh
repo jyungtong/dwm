4617157754c1824a992b5458dc5f43531b7bfe02
jyung <huntthetux@gmail.com>
2024-12-06 21:13:23 +0800

    desktop autostart

--- addons/.dwm/autostart.sh
+++ addons/.dwm/autostart.sh
@@ -6,4 +6,9 @@ xset s 600 &
 killall dwmblocks || true && dwmblocks &
 dunst &
 fcitx5 &
+flameshot &
 xrandr --output HDMI-1 --mode 1920x1080 --rate 74.97
+
+# desktop
+xrandr --output DP-2 --mode 2560x1440 --rate 84.98
+xrdb -merge ~/.Xresources
